d52277b81f4e1942998ccf35a49c6f9097882b20
user <user@JANELTON>
2024-02-21 10:16:23 -0800

    Add project files.

---
+++ ternary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
