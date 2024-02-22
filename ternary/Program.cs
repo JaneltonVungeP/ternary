100c769dd1db66f72da5234e119b9590a42443f0
JaneltonVungeP <cacuaconovavida@gmail.com>
2024-02-21 20:46:38 -0800

    my first

--- ternary/Program.cs
+++ ternary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ternary
 {
@@ -6,7 +8,43 @@ namespace ternary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<string> listas = new List<string>();
+            List<int> listasinteiras = new List<int>();
+
+            Random aleatorio = new Random();
+            string[] nomes = new string[] { "janelton", "vieira", "thokozile", "tunilson", "samuel", "gemima" };
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                int indexealeatyorio = aleatorio.Next(nomes.Length);
+                listas.Add(nomes[indexealeatyorio]);
+                listasinteiras.Add(indexealeatyorio);
+            }
+
+            foreach (var item in listas)
+            {
+                //exibir a lista
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            string finditem = listas.Find(janelton => janelton == "9");
+            Console.WriteLine("\n" + finditem);
+
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            // Contar quantas vezes "elemento2" aparece na lista
+            int contagem = listas.Count(item => item == "thokozile");
+            Console.WriteLine(contagem);  // Isto irá imprimir o número de vezes que "elemento2" aparece na lista
+
+
+
+
+
         }
     }
 }
